a4de98cec50f25d90a142a026f774c18ca9db5b1
root <femi@thepoint.africa>
2019-02-24 10:54:09 +0100

    added login route

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const multer = require("multer");
+const checkAuth = require('../middleware/check-auth');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
